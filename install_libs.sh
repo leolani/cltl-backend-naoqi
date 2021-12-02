e967c4eb37e9b65750cb0f0e0f449caadfb6b206
Thomas Baier <tbaier@math.bme.hu>
2021-12-02 11:54:43 +0100

    Remove unnecessary filter

--- install_libs.sh
+++ install_libs.sh
@@ -27,7 +27,7 @@ pip install --user virtualenv
 python -m virtualenv venv
 source venv/bin/activate
 
-ls -tr libs | grep -v virtualenv | grep -v requirements | {
+ls -tr libs | grep -v virtualenv | {
   while read i;
   do pip install "libs/$i";
   done
